03c26eaf72ab4071bd8b9c8bae5167e742fb7b8f
ilyahnuda <ilyahnuda@gmail.com>
2022-04-15 08:51:05 +0300

    feat : context checker for boolExpr

--- src/main/java/Elements/BoolExpr.java
+++ src/main/java/Elements/BoolExpr.java
@@ -1,6 +1,8 @@
 package Elements;
 
 import base.Base;
+import wallker.MyWalker;
+import wallker.Semantic;
 
 public class BoolExpr extends Base {
     private String ax;
@@ -10,30 +12,49 @@ public class BoolExpr extends Base {
     private ExpressionMath right;
     private String neg;
     private BoolExpr sub;
-    public BoolExpr(String ax){
-        this.ax=ax;
-        this.compare=null;
+
+    public BoolExpr(String ax) {
+        this.ax = ax;
+        this.compare = null;
     }
-    public BoolExpr(String Negotation,BoolExpr boolExpr){
-        this.sub=boolExpr;
-        this.ax=null;
-        this.compare=null;
-        this.neg=Negotation;
+
+    public BoolExpr(String Negotation, BoolExpr boolExpr) {
+        this.sub = boolExpr;
+        this.ax = null;
+        this.compare = null;
+        this.neg = Negotation;
     }
-    public BoolExpr(ExpressionMath left,String compare,ExpressionMath right){
-        this.left=left;
-        this.compare=compare;
-        this.right=right;
-        this.ax=null;
-        this.sub=null;
+
+    public BoolExpr(ExpressionMath left, String compare, ExpressionMath right) {
+        this.left = left;
+        Declaration left1 = new Declaration(left.type, left, null, left.getExpr());
+        if (MyWalker.declarations.size() == 0) {
+            ErrorListener.callErrorDeclaration(left1.getName());
+        }
+        if (left.kind.equals("id")) {
+            System.out.println(left1.getName());
+            Semantic.checkContext(MyWalker.declarations, left1);
+        }
+        Declaration right1 = new Declaration(right.type, right, null, right.getExpr());
+        if (MyWalker.declarations.size() == 0) {
+            ErrorListener.callErrorDeclaration(right1.getName());
+        }
+        if (right.kind.equals("id")) {
+            Semantic.checkContext(MyWalker.declarations, right1);
+        }
+        this.compare = compare;
+        this.right = right;
+        this.ax = null;
+        this.sub = null;
     }
+
     @Override
-    public String toString(){
-        StringBuilder str=new StringBuilder();
-        if(ax!=null){
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        if (ax != null) {
             return ax;
         }
-        if(sub!=null){
+        if (sub != null) {
             return str.append(neg).append("(").append(sub).append(")").toString();
         }
         return str.append(left.toString()).append(compare).append(right.toString()).toString();
--- test.g4
+++ test.g4
@@ -3,7 +3,7 @@ grammar test;
 program: (function ENDLINE)* statement+ ;
 
 // Лексемы языка
-
+WS:                 [ \t\r\n\u000C]+ -> channel(HIDDEN);
 // два тип
 INT : 'int';
 FLOAT : 'float';
@@ -19,7 +19,6 @@ TRUE:'true';
 FALSE:'false';
 RETURN: 'return';
 TAB:'    ';
-SPACE:' ';
 ENDLINE:[\n];
 NUMBER : [0-9]+;
 FLOATNUMBER:[0-9]+'.'[0-9]+;
@@ -54,10 +53,10 @@ ifHeader: IF OPEN_BRACKET boolExpr CLOSE_BRACKET;
 ifStatement: ifHeader (ENDLINE (TAB)+ statement)+ (elseStatement)? ;
 elseStatement:ELSE (ENDLINE (TAB)+ statement)+;
 //Декларация переменной и присваивание значений
-declaration : type SPACE+ ID SPACE* ASSIGN SPACE* (OPEN_BRACKET type CLOSE_BRACKET)? expressionMath ;
-assigment:ID SPACE* ASSIGN SPACE* (OPEN_BRACKET type CLOSE_BRACKET)? expressionMath;
+declaration : type  ID  ASSIGN  (OPEN_BRACKET type CLOSE_BRACKET)? expressionMath ;
+assigment:ID  ASSIGN  (OPEN_BRACKET type CLOSE_BRACKET)? expressionMath;
 // мат.выражения и конструкци которые можно передавать как аргумент
-expressionMath: expressionMath SPACE* arif SPACE* expressionMath
+expressionMath: expressionMath  arif  expressionMath
 |ID
 |br_expr
 |NUMBER
@@ -69,17 +68,17 @@ compare_op :
 EQUAL|NON_EQUAL|LESS|GREATER|LESS_OR_EQUALS|GREATER_OR_EQUALS;
 //Констркуции для функции
 functionCall:ID OPEN_BRACKET CLOSE_BRACKET
-            |ID OPEN_BRACKET(expressionMath (COMMA SPACE expressionMath)*) CLOSE_BRACKET;
-function:functionHeader ENDLINE ( TAB statement)* (TAB RETURN SPACE (expressionMath | VOID));
-functionHeader:FUNCTION SPACE+ (type|VOID) SPACE+ ID OPEN_BRACKET functionParameters CLOSE_BRACKET;
-functionParameters:(type SPACE+ ID (COMMA SPACE* type SPACE+ ID)*);
+            |ID OPEN_BRACKET(expressionMath (COMMA  expressionMath)*) CLOSE_BRACKET;
+function:functionHeader ENDLINE ( TAB statement)* (TAB RETURN  (expressionMath | VOID));
+functionHeader:FUNCTION  (type|VOID)  ID OPEN_BRACKET functionParameters CLOSE_BRACKET;
+functionParameters:(type  ID (COMMA  type  ID)*);
 //Вывод инф.
 print_expr : PRINT OPEN_BRACKET (expressionMath | boolExpr)  CLOSE_BRACKET ;
 // Цикл while
 whileCycle:whileHeader (ENDLINE (TAB)+ statement)+ ;
 whileHeader:WHILE OPEN_BRACKET boolExpr CLOSE_BRACKET;
 // логическое выражение
-boolExpr:expressionMath SPACE* compare_op SPACE* expressionMath
+boolExpr:expressionMath  compare_op  expressionMath
 | NEGATION OPEN_BRACKET boolExpr CLOSE_BRACKET
 | TRUE
 | FALSE
